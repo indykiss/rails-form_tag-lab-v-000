ae646ab7ece104fdd214485fea586808fbd30d99
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-17 02:14:43 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -7,5 +7,11 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def create
+  Student.create(first_name: params[:students][:first_name], last_name: params[:students][:last_name])
+
+  redirect_to posts_path
+end
 
 end
