5fcce98ebdbc5bba0cc0c948538036bd26cc8c4c
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-17 02:14:50 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,7 @@ class StudentsController < ApplicationController
   def create
   Student.create(first_name: params[:students][:first_name], last_name: params[:students][:last_name])
 
-  redirect_to posts_path
+  redirect_to students_path
 end
 
 end
